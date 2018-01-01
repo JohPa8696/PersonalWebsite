a6e458041ef888e3a6f0801b27bc9431993fd917
DESKTOP-2KFE49C\John Pham <dpha010@aucklanduni.ac.nz>
2018-01-01 19:45:15 +1300

    add a functional project filter

--- src/app/component/projects/projects.component.ts
+++ src/app/component/projects/projects.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from  '../../services/data.service';
 import { Project } from '../../models/Project';
+import { Observable } from 'rxjs/Rx';
+
+import * as _ from "lodash";
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -14,6 +17,13 @@ export class ProjectsComponent implements OnInit {
   languageDict =  new Map<string, Project[]>();
   frameworkDict =  new Map<string, Project[]>();
   technologyDict =  new Map<string, Project[]>();
+
+  typeList: string[];
+  languageList: string[];
+  frameworkList: string[];
+  technologyList: string[];
+
+  filters= ['','','',''];
 
   systemStyle = {
     'background-color':'#4286f4',
@@ -74,7 +84,6 @@ export class ProjectsComponent implements OnInit {
             }
           });
         }
-        console.log(this.frameworkDict);
         if(this.projects[i].technologies != undefined){
           _.each(_.range(this.projects[i].technologies.length),(j) =>{
             if (this.technologyDict.get(this.projects[i].technologies[j]) == undefined){
@@ -86,20 +95,88 @@ export class ProjectsComponent implements OnInit {
             }
           });
         }
-        console.log(this.technologyDict);
-        // Frameworks
-
-        // _.each(_.range(this.projects[i].frameworks.length),(j) =>{
-        //   if (this.frameworkDict.get(this.projects[i].languages[j]) == undefined){
-        //     this.frameworkDict.set(this.projects[i].languages[j],[this.projects[i]]);
-        //   } else {
-        //     let prjs = this.frameworkDict.get(this.projects[i].languages[j]);
-        //     prjs.push(this.projects[i]);
-        //     this.frameworkDict.set(this.projects[i].languages[j],prjs);
-        //   }
-        // });
       });
+      this.typeList = Array.from(this.typeDict.keys());
+      this.languageList = Array.from(this.languageDict.keys());
+      this.frameworkList = Array.from(this.frameworkDict.keys());
+      this.technologyList = Array.from(this.technologyDict.keys());
+      console.log(this.typeList);
     });
+    // Observable.interval(2000).subscribe(x => {
+    //   console.log(this.typeDict);
+    // });
+  }
+
+  filter(key: string, ftr:string){
+    console.log(key);
+    if(key == 'type'){
+      if(this.filters[0] == ftr){
+        this.filters[0] = '';
+        this.filteredProjects = this.project;
+      } else{
+        this.filters[0] = ftr;
+        this.filteredProjects = this.typeDict.get(ftr);
+      }
+    } else if (key == 'language'){
+      if(this.filters[1] == ftr){
+        this.filters[1] = '';
+        this.filteredProjects = this.project;
+      } else{
+        this.filters[1] = ftr;
+        this.filteredProjects = this.languageDict.get(ftr);
+      }
+    } else if (key == 'framework'){
+      if(this.filters[2] == ftr){
+        this.filters[2] = '';
+        this.filteredProjects = this.project;
+      } else{
+        this.filters[2] = ftr;
+        this.filteredProjects = this.frameworkDict.get(ftr);
+      }
+    } else {
+      if(this.filters[3] == ftr){
+        this.filters[3] = '';
+        this.filteredProjects = this.project;
+      } else{
+        this.filters[3] = ftr;
+        this.filteredProjects = this.technologyDict.get(ftr);
+      }
+    }
+
+    // let prjs = [];
+    // if(this.filters[0] == '' ){
+    //   prjs = this.projects;
+    // }else{
+    //   prjs = this.typeDict.get(this.filters[0]);
+    // }
+    //
+    // if (this.filters[1] != ''){
+    //   let temp = this.languageDict.get(this.filters[1]);
+    //   for(let i =0; i < temp.length; i++){
+    //     if(!prjs.includes(temp[i])){
+    //       prjs.push(temp[i]);
+    //     }
+    //   }
+    // }
+    //
+    // if (this.filters[2] != ''){
+    //   let temp = this.frameworkDict.get(this.filters[2]);
+    //   for(let i =0; i < temp.length; i++){
+    //     if(!prjs.includes(temp[i])){
+    //       prjs.push(temp[i]);
+    //     }
+    //   }
+    // }
+    // if (this.filters[3] != ''){
+    //   let temp = this.technologyDict.get(this.filters[3]);
+    //   for(let i =0; i < temp.length; i++){
+    //     if(!prjs.includes(temp[i])){
+    //       prjs.push(temp[i]);
+    //     }
+    //   }
+    // }
+    //
+    // this.filteredProjects = prjs;
   }
 
   getHeaderColor(type:String){
